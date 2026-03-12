a081d97abee737ffa8f4b9ae7821adb615f0e0c1
DWONeill18 <d.w.oneill.18@gmail.com>
2026-03-12 16:25:32 +0000

    Added an empty file condition check to the readFile method

--- src/main/java/ReadDNA.java
+++ src/main/java/ReadDNA.java
@@ -13,10 +13,14 @@ public class ReadDNA {
     public void readFile(String fileName) throws IOException {
 
         try (FileReader reader = new FileReader(fileName)) {
-            int ch;
-            while ((ch = reader.read()) != -1) {
-                out.print((char) ch);
+            int ch = reader.read();
+            if (ch == -1) {
+                out.println("Error: file is empty");
+                return;
             }
+            do {
+                out.print((char) ch);
+            } while ((ch = reader.read()) != -1);
         } catch (IOException e) {
             out.println("Error while reading file");
         }
