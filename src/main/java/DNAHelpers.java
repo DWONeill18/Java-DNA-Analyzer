c5d3aa571b9d011f5ca814b0d4262f9dd0862114
DWONeill18 <d.w.oneill.18@gmail.com>
2026-03-16 16:12:08 +0000

    Create dnaToCodons method to make tests pass

---
+++ src/main/java/DNAHelpers.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class DNAHelpers {
+
+    public List<String> dnaToCodons(String dna) {
+        if (dna.length() % 3 != 0) {
+            throw new IllegalArgumentException("DNA length must be divisible by 3");
+        }
+
+        List<String> codons = new ArrayList<>();
+        for (int i = 0; i < dna.length(); i += 3) {
+            codons.add(dna.substring(i, i + 3));
+        }
+        return codons;
+    }
+}
